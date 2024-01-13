d5d9cac8c4d247f996f71a4928d333a6f41d50d8
Ted Conn <tconn@salesforce.com>
2024-01-13 23:39:37 +0100

    moved everything to state machine

--- Source/main.lua
+++ Source/main.lua
@@ -4,7 +4,6 @@ import "CoreLibs/graphics"
 playdate.display.setRefreshRate(20)
 
 local gfx = playdate.graphics
-local spritelib = gfx.sprite
 local screenWidth = playdate.display.getWidth()
 local screenHeight = playdate.display.getHeight()
 
@@ -16,16 +15,8 @@ gfx.fillRect(0, 0, screenWidth, screenHeight)
 -- ! setup states
 local stateModule = import "states"
 
--- ! setup sprites
-local titleSprite = spritelib.new()
-titleSprite:setImage(gfx.image.new('images/getReady'))
-titleSprite:moveTo(screenWidth / 2, screenHeight / 2.5)
-titleSprite:setZIndex(950)
-titleSprite:addSprite()
-
 -- ! main update
 function playdate.update()
-	gfx.clear()
 	local currentState = stateModule.getCurrentState()
 	if currentState and currentState.update then
 		currentState:update()
@@ -44,8 +35,4 @@ function playdate.BButtonUp()
 	end
 end
 
--- stateModule.changeState(stateModule.getReady)
-print("Imported states from states.lua")
-for k, v in pairs(stateModule) do
-	print("Key:", k, "Value:", v)
-end
+stateModule.changeState(stateModule.getReady)
--- Source/states.lua
+++ Source/states.lua
@@ -1,3 +1,18 @@
+local gfx = playdate.graphics
+local spritelib = gfx.sprite
+
+local screenWidth = playdate.display.getWidth()
+local screenHeight = playdate.display.getHeight()
+
+local kDumbbellMeterWidth = screenWidth - 20
+
+-- ! setup sprites
+local titleSprite = spritelib.new()
+titleSprite:setImage(gfx.image.new('images/getReady'))
+titleSprite:moveTo(screenWidth / 2, screenHeight / 2.5)
+titleSprite:setZIndex(950)
+titleSprite:addSprite()
+
 local states = {}
 states.getReady = {
 	enter = function(self) 
@@ -6,6 +21,7 @@ states.getReady = {
 		titleSprite:setVisible(true)
 	end,
 	update = function(self)
+		gfx.clear()
 		spritelib.update()
 	end,
 	exit = function(self) 
@@ -15,8 +31,7 @@ states.getReady = {
 
 states.musclePulled = {
 	enter = function(self) 
-		print("starting muscle pulled state")
-		ignoreLastCrankPosition = true
+		print("starting musclePuled state")
 		titleSprite:setImage(gfx.image.new('images/gameOver'))
 		titleSprite:setVisible(true)
 	end,
@@ -24,20 +39,64 @@ states.musclePulled = {
 		spritelib.update()
 	end,
 	exit = function(self) 
-		print("exiting muscle pulled state")
+		print("exiting musclePulled state")
+	end
+}
+
+states.dumbbell = {
+	rotationCounter = 0,
+	lastCrankPosition = nil,
+	enter = function(self) 
+		self.lastCrankPosition = playdate.getCrankPosition()
+	end,
+	update = function(self)
+		gfx.clear()
+		local crankPosition = playdate.getCrankPosition()
+		local delta = crankPosition - self.lastCrankPosition
+		
+		-- Adjust for wrapping
+		if delta > 180 then  -- Crank turned counter-clockwise past 0
+			delta = delta - 360
+		elseif delta < -180 then  -- Crank turned clockwise past 359
+			delta = delta + 360
+		end
+		
+		self.rotationCounter = self.rotationCounter + delta
+		
+		-- Check for a full rotation
+		if math.abs(self.rotationCounter) >= 360 then
+			if self.rotationCounter < 0 then
+				print("rotation counter is below zero")
+			elseif self.rotationCounter > 0 then
+				self.rotationCounter = 0
+				-- pulled a muscle
+				changeState(states.musclePulled)
+			end
+			self.rotationCounter = 0  -- Reset the counter
+		end
+		
+		self.lastCrankPosition = crankPosition;
+		local fillWidth = (math.max(self.rotationCounter, 0) / 360) * kDumbbellMeterWidth
+		print("rotation counter: " .. self.rotationCounter)
+		print("fill width: " .. fillWidth)
+		gfx.setColor(gfx.kColorBlack) -- Set the color to black
+		gfx.fillRect(10, 10, fillWidth, 20)
+	end,
+	exit = function(self) 
+		print("exiting dumbbell state")
 	end
 }
 
 local currentState
 
 function changeState(newState, ...)
+	
 	print("requesting new state")
 	if currentState and currentState.exit then
 		currentState:exit()
 	end
 
-	currentState = states[newState]
-	print(currentState)
+	currentState = newState
 	if currentState and currentState.enter then
 		currentState:enter(...)
 	end
@@ -45,11 +104,6 @@ end
 
 function getCurrentState()
 	return currentState
-end
-
-print("defined states in states.lua")
-for k, v in pairs(states) do
-	print("Key:", k, "Value:", v)
 end
 
 return { getReady = states.getReady, dumbbell = states.dumbbell, musclePulled = states.musclePulled, changeState = changeState, getCurrentState = getCurrentState }
